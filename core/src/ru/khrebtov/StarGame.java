9e206648bdbf7566686d8085574e3ddd0181d0e2
framzik <framzik@gmail.com!>
2021-10-15 23:06:23 +0300

    ls2 init

---
+++ core/src/ru/khrebtov/StarGame.java
@@ -0,0 +1,13 @@
+package ru.khrebtov;
+
+import com.badlogic.gdx.Game;
+
+import ru.khrebtov.screen.MenuScreen;
+
+public class StarGame extends Game {
+
+    @Override
+    public void create() {
+        setScreen(new MenuScreen());
+    }
+}
