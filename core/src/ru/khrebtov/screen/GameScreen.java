341296ae8ef3c0c798695a6f69f32228ab8571af
framzik <framzik@gmail.com!>
2021-10-22 06:31:10 +0300

    ls4 init

--- core/src/ru/khrebtov/screen/GameScreen.java
+++ core/src/ru/khrebtov/screen/GameScreen.java
@@ -1,6 +1,5 @@
 package ru.khrebtov.screen;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
@@ -8,53 +7,29 @@ import com.badlogic.gdx.math.Vector2;
 import ru.khrebtov.base.BaseScreen;
 import ru.khrebtov.math.Rect;
 import ru.khrebtov.sprite.Background;
-import ru.khrebtov.sprite.ExitButton;
-import ru.khrebtov.sprite.PlayButton;
 import ru.khrebtov.sprite.Star;
 
 
-public class MenuScreen extends BaseScreen {
+public class GameScreen extends BaseScreen {
 
-    private static final int STAR_COUNT = 256;
-
-    private final Game game;
+    private static final int STAR_COUNT = 64;
 
     private TextureAtlas atlas;
     private Texture bg;
-
     private Background background;
+
     private Star[] stars;
-
-    private ExitButton exitButton;
-    private PlayButton playButton;
-
-    public MenuScreen(Game game) {
-        this.game = game;
-    }
 
     @Override
     public void show() {
         super.show();
-        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
         stars = new Star[STAR_COUNT];
         for (int i = 0; i < stars.length; i++) {
             stars[i] = new Star(atlas);
         }
-        exitButton = new ExitButton(atlas);
-        playButton = new PlayButton(atlas, game);
-    }
-
-    @Override
-    public void resize(Rect worldBounds) {
-        super.resize(worldBounds);
-        background.resize(worldBounds);
-        for (Star star : stars) {
-            star.resize(worldBounds);
-        }
-        exitButton.resize(worldBounds);
-        playButton.resize(worldBounds);
     }
 
     @Override
@@ -65,6 +40,15 @@ public class MenuScreen extends BaseScreen {
     }
 
     @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+    }
+
+    @Override
     public void dispose() {
         super.dispose();
         bg.dispose();
@@ -72,17 +56,23 @@ public class MenuScreen extends BaseScreen {
     }
 
     @Override
+    public boolean keyDown(int keycode) {
+        return super.keyDown(keycode);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return super.keyUp(keycode);
+    }
+
+    @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        exitButton.touchDown(touch, pointer, button);
-        playButton.touchDown(touch, pointer, button);
-        return false;
+        return super.touchDown(touch, pointer, button);
     }
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
-        exitButton.touchUp(touch, pointer, button);
-        playButton.touchUp(touch, pointer, button);
-        return false;
+        return super.touchUp(touch, pointer, button);
     }
 
     private void update(float delta) {
@@ -97,9 +87,6 @@ public class MenuScreen extends BaseScreen {
         for (Star star : stars) {
             star.draw(batch);
         }
-        exitButton.draw(batch);
-        playButton.draw(batch);
         batch.end();
     }
-
 }
---
+++ core/src/ru/khrebtov/sprite/ExitButton.java
@@ -0,0 +1,30 @@
+package ru.khrebtov.sprite;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+import ru.khrebtov.base.BaseButton;
+import ru.khrebtov.math.Rect;
+
+
+public class ExitButton extends BaseButton {
+
+    private static final float HEIGHT = 0.2f;
+    private static final float PADDING = 0.03f;
+
+    public ExitButton(TextureAtlas atlas) {
+        super(atlas.findRegion("btExit"));
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(HEIGHT);
+        setRight(worldBounds.getRight() - PADDING);
+        setBottom(worldBounds.getBottom() + PADDING);
+    }
+
+    @Override
+    public void action() {
+        Gdx.app.exit();
+    }
+}
